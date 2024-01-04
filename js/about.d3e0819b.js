d7c5177a6c8fff363acd369c2a9cb7a5a25534aa
AlexKostyk <alekskostyk2012@gmail.com>
2024-01-04 18:31:31 +0300

    Updates

---
+++ js/about.d3e0819b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkprofile"]=self["webpackChunkprofile"]||[]).push([[443],{833:function(n,e,r){r.r(e),r.d(e,{default:function(){return l}});var u=r(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),s=[a];function i(n,e){return(0,u.wg)(),(0,u.iD)("div",t,s)}var c=r(89);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
+//# sourceMappingURL=about.d3e0819b.js.map
